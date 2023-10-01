2e921ce11d9f6da4b9d9802556eae0f52b85fa50
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2023-10-01 16:06:24 +0700

    create new branch pop

--- ex-3.js
+++ ex-3.js
@@ -1,13 +1,25 @@
+// Exercise #3 Checking Plain Object Function
+// Start coding here
 function isPlainObject(value) {
-  // Start coding here
+  if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+    return true
+  } else if (Array.isArray(value) || value === null) {
+    return false
+  } else {
+    return "This is not an object"
+  }
 }
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
 let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
+let result4 = isPlainObject("Hello")
+let result5 = isPlainObject(1234)
 
-// ผลลัพธ์ควรที่ได้จาก Example case
+// ผลลัพธ์ที่ควรได้จาก Example case
 console.log(result1); //true
 console.log(result2); // false
 console.log(result3); // false
+console.log(result4); // "This is not an object"
+console.log(result5); // "This is not an object"
