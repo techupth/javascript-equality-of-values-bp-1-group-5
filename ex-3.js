72fa8a4d6ce6db3e2c2a90b4d0362eba76b959b2
Cynthy YD <cynthyyd@Cynthys-MacBook-Pro.local>
2023-10-07 12:21:09 +0700

    add: exercise answers 1-3

--- ex-3.js
+++ ex-3.js
@@ -1,13 +1,22 @@
-function isPlainObject(value) {
+function isPlainObject(x) {
   // Start coding here
+  if (typeof x === 'object' && !Array.isArray(x) && x !== null) {
+    return true
+  } else if (Array.isArray(x) || x === null) {
+    return false
+  } else {
+    return "This is not an object"
+  }
 }
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
 let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
+let result4 = isPlainObject(7000);
 
 // ผลลัพธ์ควรที่ได้จาก Example case
 console.log(result1); //true
 console.log(result2); // false
 console.log(result3); // false
+console.log(result4); // "This is not an object"
